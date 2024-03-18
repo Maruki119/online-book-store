8cc3dd077a7f2cd7b24d0224047c9fefa6ee12a5
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-03-18 13:16:02 +0700

    test connect frontend to backend and create page for test

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+const Home = () => {  
+    return (
+        <div>
+            <h1>THIS IS HOME PAGE!</h1>
+        </div>
+    );
+
+};
+
+export default Home;
