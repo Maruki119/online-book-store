aeaec1569562936979a34b7a220da6d828d92d30
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-03-18 19:09:47 +0700

    Modify Components -login -Navbar -sign_up

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import "/.login.css"
-const LoginForm = () => {
+import React, { useState , useEffect } from "react";
+import './login.css'
+
+const LoginForm = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -9,37 +10,50 @@ const LoginForm = () => {
 
   };
 
+  useEffect (() => {
+    console.log('Popup Start');
+    return () => {
+        console.log('Popup End')
+    }
+  } , []);
+
   return (
-    <div className="login-container">
-      <img className='login-logo' src={"/images/Khaoklong.png"} alt="Login Logo" />
-      <h2 className="login-title">ยินดีต้อนรับ เข้าสู่ระบบ!</h2>
-      <p className = "login-subtitle">หากมีบัญชีแล้ว สามารถเข้าสู่ระบบด้วยบัญชีเดิมได้เลย</p>
-      <input
-        type="email"
-        placeholder="อีเมลหรือชื่อผู้ใช้"
-        className="login-input"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="รหัสผ่าน"
-        className="login-input2"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <p className="login-forget">
-          <u>ลืมรหัสผ่าน</u>
-      </p>
-      <button type="submit" className="login-button">
-        เข้าสู่ระบบ
-      </button>
-      <p className="login-options">สมัครสมาชิกข้าวกล่อง e-book ด้วยอีเมล 
-        <a href="/signup" className="login-link">
-          <u>สมัครสมาชิก</u>
-        </a>
-      </p>
+    <div className="SignIn">
+        <div className="Bg-sign-in">
+          <div className="login-container">
+              <img className="cross-logo" src='./images/cross-small.png'onClick={props.onCloseSignIn}/>
+              <img className='login-logo' src= "/images/Khaoklong.png" alt="Login Logo" />
+              <h2 className="login-title">ยินดีต้อนรับ เข้าสู่ระบบ!</h2>
+              <p className = "login-subtitle">หากมีบัญชีแล้ว สามารถเข้าสู่ระบบด้วยบัญชีเดิมได้เลย</p>
+              <input
+                type="email"
+                placeholder="อีเมลหรือชื่อผู้ใช้"
+                className="login-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="รหัสผ่าน"
+                className="login-input2"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <p className="login-forget">
+                  <u>ลืมรหัสผ่าน</u>
+              </p>
+              <button type="submit" className="login-button">
+                เข้าสู่ระบบ
+              </button>
+              <p className="login-options">สมัครสมาชิกข้าวกล่อง e-book ด้วยอีเมล 
+                <a href="/signup" className="login-link">
+                  <u>สมัครสมาชิก</u>
+                </a>
+              </p>
+          </div>
+        </div>
     </div>
+    
   );
 };
 
--- frontend/src/components/sign_up.js
+++ frontend/src/components/sign_up.js
@@ -1,42 +1,45 @@
 import "./sign_up.css"
-function SignUp()
+function SignUp(props)
+
 {
     return (
-        <div className="sign-up-container">
+        <div className = "Bg-sign-up">
+            <div className="sign-up-container">
+            <img className="cross-logo" src='/images/cross-small.png'onClick={props.onCloseSignUp}/>
+                <div className="logo-App">
+                    <img src="images/Khaoklong.png"/>
+                </div>
 
-            <div className="logo-App">
-                <img src="/Khaoklong.png"/>
+                <h3>สมัครบัญชี ข้าวกล่อง E-book</h3>
+
+                <form>
+
+                    <div className="form-group">
+                        <label htmlFor="email">อีเมล </label>
+                        <input type="email" id="email" name="email" placeholder="อีเมล" required />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="name">ชื่อผู้ใช้ </label>
+                        <input type="name" id="name" name="name" placeholder="ชื่อผู้ใช้" required />
+                    </div> 
+
+                    <div className="form-group">
+                        <label htmlFor="password">รหัสผ่าน </label>
+                        <input type="password" id="password" name="password" placeholder="รหัสผ่าน" required />
+                    </div>
+                    
+                    <button type="submit" class="SignUp-Button"> สมัครสมาชิก</button>
+
+                    <div class="SignIn">
+
+                        <p>ฉันมีบัญชีข้าวกล่อง E-book อยู่แล้ว  
+                        <a href="#" class="login-link"> เข้าสู่ระบบ</a>
+                        </p>
+
+                    </div>
+                </form>
             </div>
-
-            <h3>สมัครบัญชี ข้าวกล่อง E-book</h3>
-
-            <form>
-
-                <div className="form-group">
-                    <label htmlFor="email">อีเมล </label>
-                    <input type="email" id="email" name="email" placeholder="อีเมล" required />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="name">ชื่อผู้ใช้ </label>
-                    <input type="name" id="name" name="name" placeholder="ชื่อผู้ใช้" required />
-                </div> 
-
-                <div className="form-group">
-                    <label htmlFor="password">รหัสผ่าน </label>
-                    <input type="password" id="password" name="password" placeholder="รหัสผ่าน" required />
-                </div>
-                
-                <button type="submit" class="SignUp-Button"> สมัครสมาชิก</button>
-
-                <div class="SignIn">
-
-                    <p>ฉันมีบัญชีข้าวกล่อง E-book อยู่แล้ว  
-                    <a href="#" class="login-link"> เข้าสู่ระบบ</a>
-                    </p>
-
-                </div>
-            </form>
         </div>
     );
 }
