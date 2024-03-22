9b258d136b9deaf912f1b32272f9a7a4a5d5d7e7
phuriphat20 <155616139+phuriphat20@users.noreply.github.com>
2024-03-22 16:34:31 +0700

    add HomePage and modify app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
+import HomePage from "./Homepage";
 import Home from "./components/Home";
 import List from "./components/List";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path = "/" element = {<Navbar/>}/>
+          <Route exact path = "/" element = {<HomePage/>}/>
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/Homepage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+
+function HomePage()
+{
+
+
+    return (
+        <div className="homepage-contrainer">
+            <Navbar/>
+        </div>
+
+    );
+
+}
+
+export default HomePage;
