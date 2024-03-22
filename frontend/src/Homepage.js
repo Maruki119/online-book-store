b7c24be3ae800d4c77c52b6265565f7543e0017c
Phoom_PS <Phoom289164@gmail.com>
2024-03-22 20:31:09 +0700

    update -update navbar -add promote

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Promote from "./components/Promote"
 
 function HomePage()
 {
 
 
     return (
-        <div className="homepage-contrainer">
+        <div >    
             <Navbar/>
+            <Promote/> 
         </div>
 
     );
