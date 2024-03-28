7f92377aa2687e2b5500e48f8839082a33d2de21
phuriphat20 <155616139+phuriphat20@users.noreply.github.com>
2024-03-28 23:40:04 +0700

    create Detail_Product_login_ID

--- frontend/src/components/Detail_Product_login_ID.js
+++ frontend/src/components/Detail_Product_login_ID.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Profile from "./Navbar_Login";
-import useToken from './useToken'
+import useToken from './useToken';
+import "./Detail_Product_login_ID.css"
 
 function Detail_Product_login_ID(){
     const [selectedBook, setSelectedBook] = useState(null);
@@ -24,12 +25,16 @@ function Detail_Product_login_ID(){
             <Profile token={token} removeToken = {removeToken} setToken={setToken}/>
             <div className="Detail-Product">
                 {selectedBook && (
-                    <div className="product-item">
-                        <img className="book-image" src={selectedBook.image} alt={selectedBook.title} />
-                        <h2>{selectedBook.title}</h2>
-                        <p>ผู้เขียน: {selectedBook.author}</p>
-                        <p>หมวดหมู่: {selectedBook.category}</p>
-                        <p>ราคา: {selectedBook.price} บาท</p>
+                    <div className="Books_Info-container">
+                        <div>
+                            <img className="book-image-detial" src={selectedBook.image} alt={selectedBook.title} />
+                        </div>
+                        <div className="Info">
+                            <h2>{selectedBook.title}</h2>
+                            <p>ผู้เขียน: {selectedBook.author}</p>
+                            <p>หมวดหมู่: {selectedBook.category}</p>
+                            <p>ราคา: {selectedBook.price} บาท</p>
+                        </div>
                     </div>
             )}
             </div>
