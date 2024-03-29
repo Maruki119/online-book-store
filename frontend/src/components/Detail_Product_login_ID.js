357dbd2ef987f8747a675f3636df5a7f0f3141b1
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-03-29 13:51:28 +0700

    connect button in Detail_Product_Login_ID.js -add buynow -add cart -add wishlist

--- frontend/src/components/Detail_Product_login_ID.js
+++ frontend/src/components/Detail_Product_login_ID.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Profile from "./Navbar_Login";
 import useToken from './useToken';
 import "./Detail_Product_login_ID.css"
@@ -10,8 +10,11 @@ function Detail_Product_login_ID(){
     const [selectedBook, setSelectedBook] = useState(null);
     const { token, removeToken, setToken } = useToken();
     const { bookId } = useParams();
-  
+
+    const [profileData, setProfileData] = useState(null);
+
     useEffect(() => {
+        getData();
         axios.get(`http://127.0.0.1:5000/books/${bookId}`)
             .then((response) => {
                 setSelectedBook(response.data);
@@ -20,7 +23,69 @@ function Detail_Product_login_ID(){
                 console.error("Error fetching book:", error);
             });
     }, [bookId]);
+
+    function getData() {
+        axios.get("http://127.0.0.1:5000/profile", {
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+        })
+        .then((response) => {
+        const res = response.data;
+        res.access_token && setToken(res.access_token);
+        setProfileData({
+            _id: res._id,
+            user: res.user,
+            email: res.email,
+            fullname: res.fullname,
+            password: res.password,
+            card_id: res.card_id,
+            balance: res.balance,
+            book_access: res.book_access,
+            cart: res.cart,
+            wishlist: res.wishlist
+            });
+        })
+        .catch((error) => {
+        console.error('Error fetching profile data:', error);
+        });
+    }
   
+    function onWishlist(){
+        axios.put(`http://127.0.0.1:5000/users/${profileData._id}/add_to_wishlist`, {
+            book_id: bookId
+        },{
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        }).then((response) => {
+            const message = response.data.message;
+            console.log(response.data);
+            alert(message);
+            
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          })
+    }
+
+    function onCart(){
+        axios.put(`http://127.0.0.1:5000/users/${profileData._id}/add_to_cart`, {
+            book_id: bookId
+        },{
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        }).then((response) => {
+            const message = response.data.message;
+            console.log(response.data);
+            alert(message);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        })
+    }
+
     return (
         <div className="ShowDetailProduct-contrainer">
             <Profile token={token} removeToken = {removeToken} setToken={setToken}/>
@@ -37,13 +102,15 @@ function Detail_Product_login_ID(){
                                 <p>ราคา: {selectedBook.price}.00 บาท</p>
                                 
                                 <div className="buttom-bar">
-                                    <button className="buttom-buynow" >
-                                        ซื้อเลย
-                                    </button>
-                                    <button className="buttom-cart" >
+                                    <Link to={"/checkout"} >
+                                        <button className="buttom-buynow" onClick={onCart}>
+                                            ซื้อเลย
+                                        </button>
+                                    </Link>
+                                    <button className="buttom-cart" onClick={onCart}>
                                         <img src="/images/cart.png" alt="cart" />
                                     </button>
-                                    <button className="buttom-heart">
+                                    <button className="buttom-heart" onClick={onWishlist}>
                                         <img src="/images/heart.png" alt="heart" />
                                     </button>
                                 </div>
